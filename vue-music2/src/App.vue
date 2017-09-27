520dfbd27d689426da058ba80f9045d088917217
chenli0615 <308691895@qq.com>
2017-09-27 12:32:06 +0800

    music3

---
+++ vue-music2/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    <tab></tab>
+    <keep-alive> <!-- keep-alive  将数据保存到缓存中 第二次再用的时候就不用再请求数据了 -->
+      <router-view></router-view>
+    </keep-alive>
+    
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
+
+export default{
+  components:{
+    MHeader,
+    Tab
+  }
+}
+</script>
+
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~common/stylus/variable"
+
+  #app
+    color:$color-theme
+</style>
